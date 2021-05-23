2b5c16b909ba87497911412e9da0ac1af00bb0fe
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-05-23 19:38:23 +0530

    Added 2 old snippets, added nprogress to view navigation progress indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import Head from 'next/head';
 import '../styles/globals.css';
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
