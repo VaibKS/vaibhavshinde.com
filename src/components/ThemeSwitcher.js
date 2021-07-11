ed7fda0eae704650d6d7f70f6c6642ef0c1f653a
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-07-11 18:52:11 +0530

    fix: themeswitcher use resolvedTheme instead of theme

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -2,17 +2,17 @@ import { useTheme } from 'next-themes';
 import { SunIcon, MoonIcon } from '@heroicons/react/outline';
 
 const ThemeSwitcher = ({ className }) => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   return (
     <button
       aria-label="Switch Theme"
       className={`flex items-center p-3 border-2 dark:border-gray-600 rounded-lg ${className}`}
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      {theme === 'dark' ? (
-        <MoonIcon className="w-5 h-5 fill-current" />
-      ) : (
+      {resolvedTheme === 'dark' ? (
         <SunIcon className="w-5 h-5 fill-current" />
+      ) : (
+        <MoonIcon className="w-5 h-5 fill-current" />
       )}
     </button>
   );
